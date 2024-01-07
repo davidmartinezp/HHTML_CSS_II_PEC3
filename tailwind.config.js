3709a6d230f4544826dcb64f16bde95a64434a41
david martinez <david@clautronic.es>
2024-01-07 20:11:56 +0100

    casi fin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './src/**/*.{html,js}'
+],
   theme: {
     colors: {
       'blue': '#1fb6ff',
@@ -18,12 +20,26 @@ module.exports = {
       serif: ['Merriweather', 'serif'],
     },
     extend: {
+      maxWidth: {
+        '800': '800px',
+        '300': '300px'
+      },
       spacing: {
         '8xl': '96rem',
         '9xl': '128rem',
       },
       borderRadius: {
         '4xl': '2rem',
+      },
+      colors: {
+        gray: {
+          '50': '#f9fafb',
+          '100': '#f3f4f6',
+          '200': '#e5e7eb'
+        },
+        blue: {
+            500: '#0d6efd', // Agrega tu código de color
+        },
       }
     }
   },
